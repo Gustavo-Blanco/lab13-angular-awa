315112dcc216ce9e261736aad269a9e56b788f38
gustavo blanco <gblanco@gmail.com>
2021-06-01 12:52:17 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular13';
+export class AppComponent implements OnInit {
+  angForm: FormGroup ;
+
+  caracteres: string = "Caracteres especiales como #$%&'()*+,-./:;<=>?[\]^_`{|}~";
+
+  constructor(private fb: FormBuilder){
+    this.angForm = this.fb.group({
+      nombres : [null,[Validators.required]],
+      primer_apellido : [null,[Validators.required]],
+      segundo_apellido: [null,[Validators.required]],
+      fecha_nacimiento:[null,[Validators.required]],
+      email:[null,[Validators.required,Validators.email]],
+      password:[null,[
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/),
+      ]],
+      password_confirm:[null,[]]
+    },{validators:this.checkPasswords})
+  }
+
+  checkPasswords(group: any) { // here we have the 'passwords' group
+    const password = group.get('password').value;
+    const confirmPassword = group.get('password_confirm').value;
+  
+    return password === confirmPassword ? null : { notSame: true };
+  }
+
+  ngOnInit(){}
+
+  submit(value : any)  {
+    alert(`Acaba de registrarse correctamente!`);
+  }
+  
+  onClickSubmit(email : string,password : string){
+  }
 }
